ae79d7082ffb90649d0280cc4631e6bf1525f24c
yosuatarigan <yosuatarigan@MacBook-Air-Yosua.local>
2025-12-11 14:27:24 +0700

    sudah pasang firebase

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.2")
+        classpath("com.android.tools.build:gradle:8.6.0")
+    }
+}
+
 allprojects {
     repositories {
         google()
